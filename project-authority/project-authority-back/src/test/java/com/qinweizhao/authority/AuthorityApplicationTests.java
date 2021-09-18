dc7c214844692e8f0d5d9dbc9c3b73f630b49828
YVKG <yvkg@qq.com>
2021-09-18 14:40:28 +0800

    commit

---
+++ project-authority/project-authority-back/src/test/java/com/qinweizhao/authority/AuthorityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinweizhao.authority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
